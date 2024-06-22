45e2a4c6b686a02711f5946a79f9088ef256f712
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2024-06-22 16:18:01 +0200

    URL SHORTENER
    finished the shortening url part successfully creating a random string instead of a very lengthy url and it navigates at the exact place where you took it from

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,41 @@ var shortenBtn = document.getElementById('shorten-btn');
 var urlInputElement = document.getElementById("url-input");
 var urlsListElement = document.getElementById("text-div");
 
+
+
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let result = ''
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return result
+}
+
+function getMainUrl(url) {
+    try {
+        const puttedUrl = new URL(url)
+        return puttedUrl.hostname
+    } catch (error) {
+        console.log("URL is invalid: ", error)
+        return null;
+    }
+}
+
+
 function loadUrls() {
     var urls = JSON.parse(localStorage.getItem("urls")) || [];
     urlsListElement.innerHTML = '';
     urls.forEach(url => {
-        var linkDiv = document.createElement('div');
-        linkDiv.className = 'link-div';
-        linkDiv.innerHTML = `<a href="${url}">${url}</a>
+        var mainUrl = getMainUrl(url)
+        if (mainUrl) {
+            const randomStringForUrl = generateRandomString(10)
+            var linkDiv = document.createElement('div');
+            linkDiv.className = 'link-div';
+            linkDiv.innerHTML = `<a href="${url}">${mainUrl}/${randomStringForUrl}</a>
                              <span><a href=""><img src="assets/images/deleteIcon.png" alt=""></a></span>`;
-        urlsListElement.appendChild(linkDiv);
+            urlsListElement.appendChild(linkDiv);
+        }
     });
 }
 
